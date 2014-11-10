d75b29b650b577f985d48ec9f589bfb98c38f6f5
baodvu <baodvu@gmail.com>
2014-11-09 19:38:00 -0500

    Givens Rotation

--- src/mathproject/presenters/factor/GivensRotation.java
+++ src/mathproject/presenters/factor/GivensRotation.java
@@ -3,8 +3,6 @@ package mathproject.presenters.factor;
 import java.util.ArrayList;
 import java.util.List;
 import mathproject.models.Matrix;
-import mathproject.models.Vector;
-import mathproject.presenters.MatrixOps;
 
 /**
  *
@@ -33,7 +31,44 @@ public class GivensRotation implements QRFactorization {
     @Override
     public void calculate() {
         Gs = new ArrayList<>();
-        
+        Matrix GA = A.copy();
+        int rows = A.getNumberOfRows();
+        int cols = A.getNumberOfCols();
+        for (int j = 1; j <= cols; j++) {
+            for (int i = j + 1; i <= rows; i++) {
+                if (GA.get(i, j) != 0) {
+                    Matrix G = new Matrix(rows, cols);
+                    double a = GA.get(j, j);
+                    double b = GA.get(i, j);
+                    double r = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+                    double c = a/r;
+                    double s = -b/r;
+                    System.out.println(r);
+                    System.out.println(c);
+                    System.out.println(s);
+                    for (int row = 1; row <= rows; row++) {
+                        for (int col = 1; col <= cols; col++) {
+                            double value = 0;
+                            if ((row == i && col == i) || (row == j && col == j)) {
+                                value = c;
+                            } else if (row == i && col == j) {
+                                value = s;
+                            } else if (row == j && col == i) {
+                                value = -s;
+                            } else if (row == col) {
+                                value = 1;
+                            }
+                            G.put(row, col, value);
+                        }
+                    }
+                    GA = G.multiply(GA);
+                    if (GA.get(i, j) < 0.0000000001) {
+                        GA.put(i, j, 0);
+                    }
+                    System.out.println(GA);
+                }
+            }
+        }
         
     }
 
